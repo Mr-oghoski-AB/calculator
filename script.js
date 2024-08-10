ba11873f50c328ff47c2acc8d142d12578843943
oghovese imetowa <itzoghuski@gmail.com>
2024-08-10 20:04:51 +0100

    added new features (percentage , backspace) and fixed some minor bugs

--- script.js
+++ script.js
@@ -17,7 +17,12 @@ const subtract = (...arr) => {
 // divide function
 const divide  = (...arr) => {
     let total = arr.reduce((first,second) => {
-        return (first / second).toFixed(4)
+        if (second == 0){
+            return 'lmao ;)'
+        }else{
+            return (first / second).toFixed(4)
+        }
+        
     })
     return total
 }
@@ -30,9 +35,15 @@ const multiply  = (...arr) => {
     return total
 }
 
+// percentage 
+const percent = (num) => {
+    let total = num/100
+        return total
+}
+
 let firstNum ; 
 let operator  = '';
-let secondNum ;
+let secondNum  ;
 let result ;
 
 // operate function , that evaluate two values with the operator
@@ -68,6 +79,9 @@ const display = document.querySelector('#input')
 const clear = document.querySelector('#clear')
 const sign = document.querySelectorAll('.operate')
 const equal = document.querySelector('#equalTo')
+const point = document.querySelector('.piont')
+const backSpace = document.querySelector('#back')
+const percentage = document.querySelector('#percentage')
 let answer = document.querySelector('#result')
 
 let firstOperand = '';
@@ -87,12 +101,27 @@ number.forEach(element => {
     })
 })
 
+point.addEventListener('click', () => {
+    if (!firstOperand.includes('.')){
+        firstOperand += point.id
+        display.textContent += point.id
+    }
+    else if (!secondOperand.includes('.')){
+        secondOperand += point.id
+        display.textContent += point.id
+    }
+})
 
 sign.forEach(element => {
     element.addEventListener('click', () => {
+        if (firstNum == undefined){
+            firstNum = 0
+        }
         if (typeof(result) != 'undefined'){
             operate(firstNum,operator,secondNum);
             firstNum = result
+            let string = result
+            firstOperand = string.toString()
             secondOperand = ''
             secondNum = ''
             display.textContent = firstNum
@@ -100,6 +129,8 @@ sign.forEach(element => {
         else if (typeof(result) == 'undefined' && typeof(secondNum) != 'undefined'){
             operate(firstNum,operator,secondNum);
             firstNum = result
+            let string = result
+            firstOperand = string.toString()
             secondOperand = ''
             secondNum = ''
             display.textContent = firstNum
@@ -121,10 +152,64 @@ equal.addEventListener('click', () => {
     
 })
 
+percentage.addEventListener('click' , () => {
+    if (firstNum != undefined && secondNum != undefined){
+        operate(firstNum,operator,secondNum)
+        let num = result
+        result = percent(num)
+        answer.textContent = result
+        
+    }
+    else if (firstNum != undefined && secondNum == undefined){
+        let num = firstNum
+        answer.textContent = percent(num)
+        result = percent(num)
+    }
+    else if (answer.textContent != ''){
+        let num = firstNum
+        result = percent(num)
+    }
+    
+
+})
+
+backSpace.addEventListener('click' , () => {
+    if (typeof(secondNum) == 'undefined' && operator == ''){
+        let word = firstOperand.split('')
+        word.pop()
+        firstOperand = word.join('')
+        display.textContent = firstOperand
+        firstNum = Number(firstOperand)
+        result = undefined
+        answer.textContent = ''
+        if (firstNum == 0){
+            firstNum = ''
+        }
+
+    }
+
+    else if (operator != '' && typeof(secondNum) == 'undefined' ){
+        operator = ''
+        display.textContent = firstOperand
+    }
+
+    else if (typeof(firstNum) != 'undefined' && operator != ''){
+        let word = secondOperand.split('')
+        word.pop()
+        secondOperand = word.join('')
+        display.textContent = firstOperand + ' ' + operator + ' ' + secondOperand;
+        secondNum = Number(secondOperand)
+        result = undefined
+        answer.textContent = ''
+        if (secondNum == 0){
+            secondNum = undefined
+        }
+    }
+})
 
 clear.addEventListener('click', () => {
     display.textContent = ''
-    firstNum = ''
+    firstNum = undefined
     secondNum = undefined
     operator = ''
     firstOperand = ''
